e01bd246ee42c2eb6f17caa2d3d320f875f9a9f6
Bedroom123321 <romanbadest@gmail.com>
2025-03-10 18:19:22 +0300

    lab2

--- TransportLogistics/src/main/java/com/myapp/transportlogistics/models/Truck.java
+++ TransportLogistics/src/main/java/com/myapp/transportlogistics/models/Truck.java
@@ -1,25 +1,79 @@
 package com.myapp.transportlogistics.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "trucks")
 public class Truck {
+
+    @Id
     private int id;
+    private String numberPlate;
     private int liftingCapacity;
     private int cargoVolume;
+    private String cargoType;
 
-    public Truck(int id, int cargoVolume, int liftingCapacity) {
+    public Truck() {
+    }
+
+    public Truck(String numberPlate, int id, int liftingCapacity, int cargoVolume, String cargoType) {
+        this.numberPlate = numberPlate;
         this.id = id;
-        this.cargoVolume = cargoVolume;
         this.liftingCapacity = liftingCapacity;
+        this.cargoVolume = cargoVolume;
+        this.cargoType = cargoType;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCargoType() {
+        return cargoType;
+    }
+
+    public void setCargoType(String cargoType) {
+        this.cargoType = cargoType;
+    }
+
     public int getCargoVolume() {
         return cargoVolume;
     }
 
+    public void setCargoVolume(int cargoVolume) {
+        this.cargoVolume = cargoVolume;
+    }
+
     public int getLiftingCapacity() {
         return liftingCapacity;
+    }
+
+    public void setLiftingCapacity(int liftingCapacity) {
+        this.liftingCapacity = liftingCapacity;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" +
+                "id=" + id +
+                ", numberPlate='" + numberPlate + '\'' +
+                ", liftingCapacity=" + liftingCapacity +
+                ", cargoVolume=" + cargoVolume +
+                ", cargoType='" + cargoType + '\'' +
+                '}';
     }
 }
--- TransportLogistics/src/main/java/com/myapp/transportlogistics/service/TruckService.java
+++ TransportLogistics/src/main/java/com/myapp/transportlogistics/service/TruckService.java
@@ -9,12 +9,6 @@ import org.springframework.stereotype.Service;
 public class TruckService {
     private final ArrayList<Truck> trucks = new ArrayList<>();
 
-    public TruckService() {
-        trucks.add(new Truck(1, 15000, 35));
-        trucks.add(new Truck(2, 4000, 15));
-        trucks.add(new Truck(3, 9000, 26));
-    }
-
     public List<Truck> getTrucks(int liftingCapacity, int cargoVolume) {
         ArrayList<Truck> filteredTrucks = new ArrayList<>();
 
