573a4a3a26d0f81db1d0ccc4976c22d30ab93777
Bedroom123321 <romanbadest@gmail.com>
2025-03-19 02:21:33 +0300

    lab2 remove all

--- TransportLogistics/src/main/java/com/myapp/transportlogistics/repository/DriversRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myapp.transportlogistics.repository;
-
-import com.myapp.transportlogistics.model.Driver;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriversRepository extends JpaRepository<Driver, Long> {
-    Optional<Driver> findByPhoneNumber(String phoneNumber);
-}
